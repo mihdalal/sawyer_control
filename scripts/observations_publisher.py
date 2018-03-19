81573bc8d0cec546a67c2a3d90b19a5731e483cc
Murtaza Dalal <mdalal@berkeley.edu>
2018-03-18 19:49:40 -0700

    added observation msg and observation publisher

---
+++ scripts/observations_publisher.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import rospy
+from sawyer_control.msg import observations
+import intera_interface as ii
+
+def talker():
+    pub = rospy.Publisher('observations', observations, queue_size=10)
+    rospy.init_node('observer', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    arm = ii.Limb('right')
+    while not rospy.is_shutdown():
+        angles_dict = arm.joint_angles()
+        joint_names = arm.joint_names()
+        angles = [
+            angles_dict[joint] for joint in joint_names
+        ]
+        velocities_dict = arm.joint_velocities()
+        velocities = [
+            velocities_dict[joint] for joint in joint_names
+        ]
+        torques_dict = arm.joint_efforts
+        torques = [
+            torques_dict[joint] for joint in joint_names
+        ]
+        state_dict = arm.endpoint_pose()
+        pos = state_dict['position']
+        orientation = state_dict['orientation']
+        endpoint_pose = [
+                pos.x,
+                pos.y,
+                pos.z,
+                orientation.x,
+                orientation.y,
+                orientation.z,
+                orientation.w
+            ]
+        pub.publish(angles, velocities, torques, endpoint_pose)
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
