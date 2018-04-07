c216e1313e49a24cb7e6001cc684d97f7c2062c3
Murtaza Dalal <mdalal@berkeley.edu>
2018-04-07 15:25:57 -0700

    added __init__.py and jacobian client

---
+++ scripts/jacobian_client.py
@@ -0,0 +1,44 @@
+import rospy
+from robot_info.srv import *
+import numpy as np
+import ipdb
+joint_names = [
+    '_l2',
+    '_l3',
+    '_l4',
+    '_l5',
+    '_l6',
+    '_hand'
+]
+
+def get_pose_jacobian(poses, jacobians):
+    pose_jacobian_dict = {}
+    counter = 0
+    pose_counter = 0
+    jac_counter = 0
+    for i in range(len(joint_names)):
+        pose = poses[pose_counter:pose_counter+3]
+        jacobian = np.array([
+            jacobians[jac_counter + 3:jac_counter + 10],
+            jacobians[jac_counter + 10:jac_counter + 17],
+            jacobians[jac_counter + 17:jac_counter+ 24],
+        ])
+        pose_counter += 3
+        jac_counter += 21
+        pose_jacobian_dict['right' + joint_names[counter]] = [pose, jacobian]
+        counter += 1
+    return pose_jacobian_dict
+
+def get_robot_pose_jacobian_client(name):
+    rospy.wait_for_service('get_robot_pose_jacobian')
+    try:
+        # ipdb.set_trace()
+        get_robot_pose_jacobian = rospy.ServiceProxy('get_robot_pose_jacobian', getRobotPoseAndJacobian, persistent=True)
+        resp = get_robot_pose_jacobian(name)
+        d = get_pose_jacobian(resp.poses, resp.jacobians)
+        ipdb.set_trace()
+    except rospy.ServiceException as e:
+        print(e)
+
+if __name__ == "__main__":
+    print(get_robot_pose_jacobian_client('right'))
