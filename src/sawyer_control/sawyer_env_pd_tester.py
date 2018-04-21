d11d010b23b3d6d4a39e96d0fb06c69c81b6c1db
Murtaza Dalal <mdalal@berkeley.edu>
2018-04-21 09:34:05 -0700

    improved pd tester

--- src/sawyer_control/sawyer_env_pd_tester.py
+++ src/sawyer_control/sawyer_env_pd_tester.py
@@ -1,23 +1,23 @@
 from sawyer_reaching import SawyerXYZReachingEnv
 import numpy as np
 
-desired = [0.97711039, .5, 0.27901027]
-env = SawyerXYZReachingEnv(desired=desired, reward='norm', safety_box=True, action_mode='position')
+env = SawyerXYZReachingEnv(reward='norm', safety_box=True, action_mode='position')
 total = 0
-num_steps = 5
+num_steps = 10
 env.reset()
 error = 0
-num_desireds = 10
+num_desireds = 1
+num_trials = 5
 desireds = [np.random.uniform(env.safety_box_lows, env.safety_box_highs, size=(1, 3))[0] for i in range(num_desireds)]
 for desired in desireds:
     dist = 0
-    for i in range(3):
+    env.desired = desired
+    for i in range(num_trials):
         env.reset()
         for j in range(num_steps):
             original = env._end_effector_pose()[:3]
             act = desired - original
             env._act(act)
-
         current = env._end_effector_pose()[:3]
         dist += np.linalg.norm(desired-current)
-    print(dist/3)
+    print(dist/num_trials)
