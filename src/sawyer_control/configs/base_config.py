e2928b63ab7c64a689382c77564531d9ac332c47
Murtaza Dalal <mdalal@berkeley.edu>
2018-06-27 13:07:56 -0700

    moved all the joint info to config

--- src/sawyer_control/configs/base_config.py
+++ src/sawyer_control/configs/base_config.py
@@ -1,5 +1,7 @@
 import numpy as np
-joint_names = ['right_j0',
+
+#JOINT INFO
+JOINT_NAMES = ['right_j0',
                'right_j1',
                'right_j2',
                'right_j3',
@@ -7,5 +9,61 @@ joint_names = ['right_j0',
                'right_j5',
                'right_j6'
                ]
-reset_angles = np.ones(7) #TODO: SET THIS TO A REASONABLE POSITION 
-reset_dict = dict(zip(joint_names, reset_angles))
+RESET_ANGLES = np.ones(7) #TODO: SET THIS TO A REASONABLE POSITION
+RESET_DICT = dict(zip(JOINT_NAMES, RESET_ANGLES))
+
+#SPACES
+JOINT_ANGLES_HIGH = np.array([
+    1.70167993,
+    1.04700017,
+    3.0541791,
+    2.61797006,
+    3.05900002,
+    2.09400001,
+    3.05899961
+])
+
+JOINT_ANGLES_LOW = np.array([
+    -1.70167995,
+    -2.14700025,
+    -3.0541801,
+    -0.04995198,
+    -3.05900015,
+    -1.5708003,
+    -3.05899989
+])
+
+JOINT_VEL_HIGH = 2*np.ones(7)
+JOINT_VEL_LOW = -2*np.ones(7)
+
+MAX_TORQUES = 0.5 * np.array([8, 7, 6, 5, 4, 3, 2])
+
+JOINT_TORQUE_HIGH = MAX_TORQUES
+JOINT_TORQUE_LOW = -1*MAX_TORQUES
+
+JOINT_VALUE_HIGH = {
+    'position': JOINT_ANGLES_HIGH,
+    'velocity': JOINT_VEL_HIGH,
+    'torque': JOINT_TORQUE_HIGH,
+}
+JOINT_VALUE_LOW = {
+    'position': JOINT_ANGLES_LOW,
+    'velocity': JOINT_VEL_LOW,
+    'torque': JOINT_TORQUE_LOW,
+}
+
+END_EFFECTOR_POS_LOW = -1.2 * np.ones(3)
+END_EFFECTOR_POS_HIGH = 1.2 *np.ones(3)
+
+END_EFFECTOR_ANGLE_LOW = -1*np.ones(4)
+END_EFFECTOR_ANGLE_HIGH = np.ones(4)
+
+END_EFFECTOR_VALUE_LOW = {
+    'position': END_EFFECTOR_POS_LOW,
+    'angle': END_EFFECTOR_ANGLE_LOW,
+}
+
+END_EFFECTOR_VALUE_HIGH = {
+    'position': END_EFFECTOR_POS_HIGH,
+    'angle': END_EFFECTOR_ANGLE_HIGH,
+}
