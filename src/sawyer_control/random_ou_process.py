24dd4833ed21ab1384546f88accd4217a85f76a2
Murtaza Dalal <mdalal@berkeley.edu>
2018-06-27 15:20:56 -0700

    added wrappers and collection tools

--- src/sawyer_control/random_ou_process.py
+++ src/sawyer_control/random_ou_process.py
@@ -6,12 +6,15 @@ import cv2
 env = SawyerXYPushingEnv(action_mode='position', reward='norm', pause_on_reset=True)
 env = ImageEnv(env)
 ou = OUStrategy(env.action_space)
-env._end_effector_pose()[:3]
 env.reset()
 def collect_data(folder_name, log_dir = '/tmp/', N=10000, rollout_len = 50):
     dir = log_dir + folder_name + '/'
     for i in range(N):
-        env.step(ou.raw)
+        env.step(ou.get_action_from_raw_action(np.zeros(3)))
+        img = env._get_image_data()
+        cv2.imwrite(dir + str(i) + '.png', img)
+        if i % rollout_len == 0:
+            env.reset()
 
 
 
