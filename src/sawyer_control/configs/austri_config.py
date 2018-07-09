eaa48ba0c25b59005830ab7d131fb02e9df6aa5d
Murtaza Dalal <mdalal@berkeley.edu>
2018-07-09 13:18:40 -0700

    fixed more config bugs

--- src/sawyer_control/configs/austri_config.py
+++ src/sawyer_control/configs/austri_config.py
@@ -1,4 +1,6 @@
 from sawyer_control.configs.base_config import *
+import numpy as np
+from gym.spaces import Box
 TORQUE_SAFETY_BOX_LOWS = np.array([0.3, -0.4, 0.2])
 TORQUE_SAFETY_BOX_HIGHS = np.array([0.7, 0.4, 0.7])
 TORQUE_SAFETY_BOX = Box(TORQUE_SAFETY_BOX_LOWS, TORQUE_SAFETY_BOX_HIGHS)
--- src/sawyer_control/configs/config.py
+++ src/sawyer_control/configs/config.py
@@ -1,7 +1,8 @@
-from sawyer_control.configs import *
-
+import sawyer_control.configs.austri_config as austri_config
+import sawyer_control.configs.base_config as base_config
+import sawyer_control.configs.ros_config as ros_config
 config_dict = dict(
-    ros_config=configs.ros_config,
-    base_config=configs.base_config,
-    austri_config=configs.austri_config
+    ros_config=ros_config,
+    base_config=base_config,
+    austri_config=austri_config
 )
