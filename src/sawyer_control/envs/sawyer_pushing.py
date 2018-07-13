9aacec2fb7b5c8edf1a1e032f05b984c28886dec
Murtaza Dalal <mdalal@berkeley.edu>
2018-07-13 13:04:10 -0700

    changed resets

--- src/sawyer_control/envs/sawyer_pushing.py
+++ src/sawyer_control/envs/sawyer_pushing.py
@@ -10,6 +10,7 @@ class SawyerXYPushingImgMultitaskEnv(SawyerEnvBaseImage):
                  reward_type='hand_distance',
                  pause_on_reset=True,
                  action_mode='position',
+                 z=.23128,
                  **kwargs
                 ):
         Serializable.quick_init(self, locals())
@@ -18,40 +19,36 @@ class SawyerXYPushingImgMultitaskEnv(SawyerEnvBaseImage):
         self.indicator_threshold = indicator_threshold
         self.reward_type = reward_type
         self._goal = None
+        self.pause_on_reset=pause_on_reset
+        self.z = z
 
     @property
     def goal_dim(self):
         return 4 #xy for object position, xy for end effector position
 
     def set_to_goal(self, goal):
-        print('pause')
-        input()
-        print('setting goal')
+        print('moving arm to desired object goal')
         obj_goal = np.concatenate((goal[:2], [self.z]))
         ee_goal = np.concatenate((goal[2:4], [self.z]))
-        #PAUSE FOR INPUT:
-        self.thresh=False
         self._position_act(obj_goal-self._end_effector_pose()[:3])
         input()
+        print('place object at end effector location and press enter')
+        #MOVES ROBOT ARM TO GOAL POSITION:
         self._position_act(ee_goal - self._end_effector_pose()[:3])
         print('setting ee')
-        input()
-        #PAUSE FOR INPUT
-        #PLACE OBJECT AT GOAL POSITION
-        #MOVES ROBOT ARM TO GOAL POSITION:
         self._goal = self._get_obs()
-        self.thresh = True
+
+    def _reset_robot(self):
+        self._act(self.reset_position - self._end_effector_pose()[:3])
+        if self.pause_on_reset:
+            input()
+            print('move object to reset position and press enter')
 
     def reset(self):
-        self.thresh = False
-        self._act(self.reset_position  - self._end_effector_pose()[:3])
-        self.thresh = True
-        if self.pause_on_reset:
-            #PAUSE AND MOVE OBJECT TO ITS RESET POSITION
-            print('in reset')
-            input()
-        observation = self._get_observation()
-        return observation
+        self._reset_robot()
+        self._goal = self.sample_goal()
+        return self._get_obs()
 
     def get_diagnostics(self, paths, prefix=''):
         return OrderedDict()
+
