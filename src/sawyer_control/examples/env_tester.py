0ef52e0a6ecd7f517ba5a5501c0655e13aa0c7e1
Murtaza Dalal <mdalal@berkeley.edu>
2019-01-07 19:29:33 -0800

    saving settings

--- src/sawyer_control/examples/env_tester.py
+++ src/sawyer_control/examples/env_tester.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 from sawyer_control.envs.sawyer_door import SawyerDoorEnv
 env = SawyerDoorEnv()
-env.reset()
+# env.reset()
 print(env._get_joint_angles())
 print(env._get_obs()[14:])
