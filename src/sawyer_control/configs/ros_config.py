35d0a3aa7957ae0b410975e634f77266c4a0eb8f
Murtaza Dalal <mdalal@berkeley.edu>
2019-01-18 21:22:33 -0800

    setup reacher exp

--- src/sawyer_control/configs/ros_config.py
+++ src/sawyer_control/configs/ros_config.py
@@ -14,39 +14,14 @@ JOINT_NAMES = ['right_j0',
                'right_j6'
                ]
 LINK_NAMES = ['right_l2', 'right_l3', 'right_l4', 'right_l5', 'right_l6', '_hand']
+
+POSITION_RESET_POS = np.array([0.6211303, 0.0516888, 0.3437545])
 RESET_ANGLES = np.array(
-    [-2.06738291e-03, - 1.17774510e+00, - 2.17089849e-03,  2.17807722e+00,
-     - 4.32617177e-04,  5.66576183e-01,  3.31357336e+00]
+    [0.15519531,  0.91280955, - 2.02724218,  1.66523147, - 2.41472554,  1.15144241, - 2.47620893]
 )
 RESET_DICT = dict(zip(JOINT_NAMES, RESET_ANGLES))
-
-POSITION_RESET_POS = np.array([ 0.55, -0.05,  0.25471401])
-
-# POSITION_CONTROL_EE_ORIENTATION=Quaternion(
-# 		x=0.70833379,
-# 		y=0.70586246,
-# 		z=-0.00452118,
-# 		w=-0.00097029,
-# 	)
-
-RESET_ANGLES = np.array(
-    [0.20223731,  0.85522556, - 2.12354875,  1.59621096, - 2.52368164,  1.01539946, -2.40745807]
-)
-
-
-
-RESET_DICT = dict(zip(JOINT_NAMES, RESET_ANGLES))
-
-# POSITION_CONTROL_EE_ORIENTATION=Quaternion(
-# 		x=0.70833379,
-# 		y=0.70586246,
-# 		z=-0.00452118,
-# 		w=-0.00097029,
-# 	)
-
 POSITION_CONTROL_EE_ORIENTATION=Quaternion(
     x=0.72693193, y=-0.03049006, z=0.6855942, w=-0.02451418
 )
-
 JOINT_SPACE_IMPD_MAX_TORQUES = 0.5 * np.array([8, 7, 6, 5, 4, 3, 2])
 JOINT_SPACE_IMPD_RATE = 1000
