85714eba563d921b6718b68ecc01f2708c5dcf8f
Murtaza Dalal <mdalal@berkeley.edu>
2019-03-01 14:11:43 -0800

    updated env test

--- src/sawyer_control/examples/env_tester.py
+++ src/sawyer_control/examples/env_tester.py
@@ -5,7 +5,7 @@ env = SawyerDoorEnv(
     action_mode='position',
     config_name='austri_config',
     reset_free=False,
-	position_action_scale=0.05,
+	position_action_scale=0.01,
 	max_speed=0.2,
 	use_compliant_position_controller=True
 )
